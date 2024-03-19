8ef5b520022dfd526d54aea84b33c89d5c612ff2
Falko Mathews <falko.mathews@iav.de>
2024-03-19 11:29:15 +0100

    example checkin

---
+++ example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+
+	// intialisation
+    int int_num1 = 10;
+    int int_num2 = 0;
+
+    int int_result = int_num1 / int_num2;
+
+    std::cout << "The result is: " << int_result << std::endl;
+
+	// successful return code
+    return 0;
+
+}
